4f1b8be28d30724da8b865b9e934752193020586
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2023-12-15 08:39:38 -0300

    Created entities repository interfaces

---
+++ CleanArchMvc/CleanArchMvc.Domain/Interfaces/Base/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArchMvc.Domain.Interfaces.Base
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> InsertAsync(T obj);
+        Task<T> UpdateAsync(T obj);
+        Task DeleteAsync(T obj);
+    }
+}
---
+++ CleanArchMvc/CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository : Base.IRepository<Product>
+    {
+        Task<Product> GetProductCategoryAsync(int id);
+    }
+}
