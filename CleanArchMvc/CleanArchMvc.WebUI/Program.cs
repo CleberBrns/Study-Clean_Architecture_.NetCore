f0c456c84901c8d069ae016c6da9138a76e636ec
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2023-12-16 20:39:43 -0300

    Created Startup file withing Service Configuration set

--- CleanArchMvc/CleanArchMvc.WebUI/Program.cs
+++ CleanArchMvc/CleanArchMvc.WebUI/Program.cs
@@ -1,9 +1,16 @@
+using CleanArchMvc.WebUI;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services); // calling ConfigureServices method
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+startup.Configure(app, builder.Environment); // calling Configure method
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
---
+++ CleanArchMvc/CleanArchMvc.WebUI/Startup.cs
@@ -0,0 +1,36 @@
+﻿using CleanArchMvc.Infra.IoC;
+
+namespace CleanArchMvc.WebUI
+{
+    public class Startup
+    {
+        public IConfiguration ConfigRoot
+        {
+            get;
+        }
+        public Startup(IConfiguration configuration)
+        {
+            ConfigRoot = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddInfrastructure(ConfigRoot);
+            services.AddRazorPages();
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapRazorPages();
+            app.Run();
+        }
+    }
+}
